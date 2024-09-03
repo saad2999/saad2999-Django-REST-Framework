7c0876f0b53ed0b91eb2df85b69a91c0ebce71e6
DESKTOP-DVQ765F\Maaz <saad586305@gmail.com>
2024-09-03 11:29:28 +0500

    Custom Permission

---
+++ gs20/api/views.py
@@ -0,0 +1,11 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication 
+from .custompermission import MyPermission
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [MyPermission]
